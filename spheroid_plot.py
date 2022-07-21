d3b800ae3c2b6fbd84dbc1b2a2a1f1f4a0f8c6c8
Koushouu <44732917+Koushouu@users.noreply.github.com>
2022-07-21 09:50:37 +0800

    Add files via upload

---
+++ spheroid_plot.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+##################################### Inputs ###########################################################################
+cell_stats_npy = r"C:\Users\CBC\PycharmProjects\pythonProject\spheroid\cell_stats_parallel_mask.npy"
+coordname = 'allcells_bright_mask.csv'
+########################################################################################################################
+# Load data
+cellstats = np.load(cell_stats_npy, allow_pickle=True).item()
+# Plot the cell voxel size distribution
+plt.hist(cellstats['cell_size'], 80)
+plt.show()
+
+# Input the minimum cell voxel size
+print('Enter the minimum cell size:')
+min_cell_size = int(input())
+cells_right_size = np.array(cellstats['cell_size']) > min_cell_size
+print('There are ' + str(sum(cells_right_size)) + ' cells with size over the minimum cell size')
+
+# Plot the nano particle intensity distribution
+nanoptl_int_array = np.array(cellstats['nanoptl_int'])
+nanoptl_int_array = nanoptl_int_array[cells_right_size]
+plt.hist(nanoptl_int_array, 150)
+plt.show()
+print('Enter the intensity threshold:')
+int_threshold = int(input())
+abv_thr = sum(nanoptl_int_array >= int_threshold)
+bel_thr = sum(nanoptl_int_array < int_threshold)
+print('There are ' + str(abv_thr) + ' cells above the intensity threshold')
+print('And there are ' + str(bel_thr) + ' cells below the intensity threshold')
+# Get x y z coordinates of the desired cells
+# Logical array of cells with high intensity
+cells_right_intensity = np.array(cellstats['nanoptl_int']) > int_threshold
+# AND with the logical array of cells with right size
+target_cells = np.logical_and(cells_right_intensity, cells_right_size)
+# Export x, y, z
+x_loc = np.array(cellstats['cell_loc_x'])
+x_loc = x_loc[target_cells]
+y_loc = np.array(cellstats['cell_loc_y'])
+y_loc = y_loc[target_cells]
+z_loc = np.array(cellstats['cell_loc_z'])
+z_loc = z_loc[target_cells]
+
+coord = np.transpose([z_loc, y_loc, x_loc])
+np.savetxt(coordname, coord, delimiter=",")
